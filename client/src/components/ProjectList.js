b13169d0aacae5dab87239a07eb767bd4575f920
Amy Kate <amykateoc6@gmail.com>
2023-07-10 21:59:50 -0400

    FINALLY fixed project rendering

--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -4,69 +4,70 @@ import { GET_PROJECTS } from '../utils/queries';
 import { useQuery } from '@apollo/client';
 import Auth from '../utils/auth';
 
-
-// {
-//   projects,
-//   projectTitle,
-//   showTitle = true,
-//   showUsername = true,
-// }
 const ProjectList = () => {
-  const showProjects = useQuery(GET_PROJECTS);
-  console.log(showProjects);
-  if (!showProjects.data.projects.length) {
+  const { loading, error, data } = useQuery(GET_PROJECTS);
+  
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  
+  const projects = data.projects;
+
+  if (!projects.length) {
     return (
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <h3> No Projects to Show </h3>
+        <h3>No Projects to Show</h3>
       </div>
     );
   }
 
-
   return (
     <div>
-      {<h3>{showProjects.projectTitle}</h3>}
-      {showProjects &&
-        showProjects.map((project) => (
-          <div key={showProjects._id} className="card mb-3">
-            <h4 className="card-header bg-primary text-light p-2 m-0">
-              { (
-                <Link
-                  className="text-light"
-                  to={`/profiles/${project.username}`}
-                >
-                  {project.username} <br />
-                  <span style={{ fontSize: '1rem' }}>
-                    posted this project
-                  </span>
-                </Link>
-              )  (
-                <>
-                  <span style={{ fontSize: '1rem' }}>
-                    You posted this project
-                  </span>
-                </>
-              )}
-            </h4>
-            <div className="card-body bg-light p-2">
-              <p>{project.projectTitle}</p>
-            </div>
-            <div className="card-body bg-light p-2">
-              <p>{project.projectDescription}</p>
-            </div>
-            <div>
-            <input type="number" id="quantity" name="quantity" min="1">
-              </input>
-              <button>Donate</button>
-            </div>
-            <Link
-              className="btn btn-primary btn-block btn-squared"
-              to={`/projects/${project._id}`}
-            >
-              Donate to this project!
-            </Link>
+      <h3>Projects</h3>
+      {projects.map((project) => (
+        <div key={project._id} className="card mb-3">
+          <h4 className="card-header bg-primary text-light p-2 m-0">
+            {Auth.loggedIn() && (
+              <Link
+                className="text-light"
+                to={`/profiles/${project.username}`}
+              >
+                {project.username} <br />
+                <span style={{ fontSize: '1rem' }}>
+                  posted this project
+                </span>
+              </Link>
+            )}
+            {!Auth.loggedIn() && (
+              <>
+                <span style={{ fontSize: '1rem' }}>
+                  You posted this project
+                </span>
+              </>
+            )}
+          </h4>
+          <div className="card-body bg-light p-2">
+            <p>{project.projectTitle}</p>
           </div>
-        ))}
+          <div className="card-body bg-light p-2">
+            <p>{project.projectDescription}</p>
+          </div>
+          <div>
+            <input type="number" id="quantity" name="quantity" min="1" />
+            <button>Donate</button>
+          </div>
+          <Link
+            className="btn btn-primary btn-block btn-squared"
+            to={`/projects/${project._id}`}
+          >
+            Donate to this project!
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
